b59d910b5e2954804a12a4c651b997eb9ea2ba98
Jackson Cribar <71723654+JacksonxCribar@users.noreply.github.com>
2021-10-22 01:08:03 -0400

    addedsomejs

--- js/main.js
+++ js/main.js
@@ -2,6 +2,34 @@
 
     // add stuff below here
 
+    function getData() {
+        fetch("./data.json")
+        .then(res => res.json)()
+        .then(data => {
+            favouriteThing(data);
+
+        })
+        .catch(error => console.error(error));
+    }
+
+    function favouriteThing(info) {
+        
+        const fav = Object.keys(info);
+    }
+
+    fav.forEach(things => {
+        let panel = favTemplate.cloneNode(true)
+        let favInfo = panel.firstElementChild.children;
+
+        favInfo[0] .querySelector('img').src = `images/${info[things].favImage}`;
+        favInfo[1].textContent = info[things].title;
+        favInfo[2].textContent = info[Things].description;
+
+    
+        theFav.appendChild(panel);
+
+    })
+
 
 
 
